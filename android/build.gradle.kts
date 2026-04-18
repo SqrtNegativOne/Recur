9a2ec982a22668f1456fa97f32f447f4673cb399
SqrtNegativOne <sqrtnegativ1@gmail.com>
2026-04-18 15:09:49 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,17 @@ allprojects {
     }
 }
 
+subprojects {
+    afterEvaluate {
+        val lib = extensions.findByType(com.android.build.gradle.LibraryExtension::class.java) ?: return@afterEvaluate
+        if (lib.namespace != null) return@afterEvaluate
+        val manifest = File(projectDir, "src/main/AndroidManifest.xml")
+        if (!manifest.exists()) return@afterEvaluate
+        val match = Regex("""package="([^"]+)"""").find(manifest.readText()) ?: return@afterEvaluate
+        lib.namespace = match.groupValues[1]
+    }
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
